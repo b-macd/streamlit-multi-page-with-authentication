09ca6e63c83c85a95597180c37a972663d9894ad
Ben MacDonald <benjamin.t.macdonald1@gmail.com>
2025-01-27 22:26:24 -0500

    Initial push with all base files

---
+++ pages/App_1.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+config_file_path = st.secrets.config_file_path
+with open(config_file_path) as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+if st.session_state['authentication_status']:
+    with st.sidebar:
+        authenticator.logout()
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        st.write(f'Current Role: *{st.session_state["roles"][0]}*')
+elif st.session_state['authentication_status'] is False:
+    st.switch_page('pages/User_Login.py')
+elif st.session_state['authentication_status'] is None:
+    st.switch_page('pages/User_Login.py')
+
+st.title('This is App 1')
+
+st.divider()
+
