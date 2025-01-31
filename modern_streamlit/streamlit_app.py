32fc09bb78d3eab51bb6b975966cec6d7b5ef950
Ben MacDonald <benjamin.t.macdonald1@gmail.com>
2025-01-30 22:37:17 -0500

    added new reset password widget

--- modern_streamlit/streamlit_app.py
+++ modern_streamlit/streamlit_app.py
@@ -55,11 +55,44 @@ def logout():
         st.session_state.pop(key)
     st.rerun()
 
+def reset_password():
+    reset_pw_form = st.form('Reset Password')
+    with reset_pw_form:
+        username = st.text_input('Username')
+        email = st.text_input('Email')
+        first = st.text_input('First Name')
+        last = st.text_input('Last Name')
+        new_pw = st.text_input('New Password', type='password')
+        confirm_new_pw = st.text_input('Confirm Password', type='password')
+        if reset_pw_form.form_submit_button():     
+            if username in config['credentials']['usernames']:
+                adhoc_list = []
+                for entry in config['credentials']['usernames'][username].values():
+                    adhoc_list.append(entry) 
+                #st.write(adhoc_list)  
+                if email == adhoc_list[0] and first == adhoc_list[2] and last == adhoc_list[3]:
+                    if confirm_new_pw == new_pw:
+                        hashed_pw = stauth.Hasher.hash(new_pw) 
+                        config['credentials']['usernames'][username]['password'] = hashed_pw
+                        with open(config_file_path, 'w') as file:
+                            yaml.dump(config, file, default_flow_style=False)
+
+                        st.success('Password Reset')
+                    else:
+                        st.error('Passwords do not match')  
+                else:
+                    st.error('The email, first name, or last name do not match our records')  
+
+            else:
+                st.error('That username does not exist')
+
+
 
 role = st.session_state.roles[0]
 
 registration_page = st.Page(registration, title='New User Registration', icon = ':material/person_add:')
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+reset_password_page = st.Page(reset_password, title='Reset Password')
 #settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
 home_page  = st.Page("landingpage/home_page.py",
     title="Home Page",
@@ -82,7 +115,7 @@ admin_1 = st.Page(
     icon=":material/security:",
 )
 
-account_page = [logout_page, home_page]
+account_page = [logout_page, home_page, reset_password_page]
 application_pages = [app_1, app_2]
 admin_pages = [admin_1]
 
